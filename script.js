ea1cd0356ade2cc472a5ffe768c996423db410f5
Minhazul Islam <minhazulislam@hotmail.com>
2022-10-16 20:50:48 +0100

    if else statement for wrong input

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function generatePassword(){ // when I click the red button
   let inputData = parseInt(window.prompt("how many characters do you want password to be?")); //an prompt requesting an input of an integer "parseInt" which will be stored inside a variable called "inputData"
 
+  if(isNaN(inputData)){ //if the data inputted is not a number "isNan"
+    window.alert("must be an integer (whole number)"); // the following message will be displayed
+    return // and due to not meeting the criteria, will be returned to to the top of the function
+  } else if(inputData < 8 || inputData > 128) { // if the input is NOT an integer between 8 & 128
+    window.alert ("must be an integer between 8  and 128 characters");// the following message will be displayed
+    return // will be returned to to the top of the function
+  }
 
 
 
-
+}
 
   // Write password to the #password input
 function writePassword() {
