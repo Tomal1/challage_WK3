b06a4d12581dc07aec8d75636855394a9a0d7d59
Minhazul Islam <minhazulislam@hotmail.com>
2022-10-16 20:46:23 +0100

    making first input prompt

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+function generatePassword(){ // when I click the red button
+  let inputData = parseInt(window.prompt("how many characters do you want password to be?")); //an prompt requesting an input of an integer "parseInt" which will be stored inside a variable called "inputData"
 
-// Write password to the #password input
+
+
+
+
+
+  // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
